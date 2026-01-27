e93f528e192d8617b5783eae87614a6e09e50584
Raj Majhi <rajm46921@gmail.com>
2026-01-28 00:38:59 +0530

    Update matplotlib.py

--- matplotlib.py
+++ matplotlib.py
@@ -201,3 +201,153 @@ plt.plot(x, y2,color="blue", **rx)
 
 plt.show()
 
+#Label
+import matplotlib.pyplot as plt
+import numpy as np
+
+x= np.array ([2023, 2024, 2025, 2026])
+y1= np.array ([15, 25, 30, 20])
+y2= np.array ([17, 23, 38, 5])
+y3= np.array ([13, 15, 20, 30])
+
+plt.title("Class size", fontsize=20, 
+          family="Arial", 
+          fontweight="bold",
+          color="green"
+         )
+
+
+
+plt.plot(x, y1)
+plt.plot(x, y2)
+plt.plot(x, y3)
+
+plt.show()
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x= np.array ([2023, 2024, 2025, 2026])
+y1= np.array ([15, 25, 30, 20])
+y2= np.array ([17, 23, 38, 5])
+y3= np.array ([13, 15, 20, 30])
+
+plt.title("Class size", fontsize=20, 
+          family="Arial", 
+          fontweight="bold",
+          color="green"
+         )
+
+plt.xlabel("Year",fontsize=20, 
+          family="Arial", 
+          fontweight="bold",
+          color="blue")
+
+
+plt.plot(x, y1)
+plt.plot(x, y2)
+plt.plot(x, y3)
+
+plt.show()
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x= np.array ([2023, 2024, 2025, 2026])
+y1= np.array ([15, 25, 30, 20])
+y2= np.array ([17, 23, 38, 5])
+y3= np.array ([13, 15, 20, 30])
+
+plt.title("Class size", fontsize=20, 
+          family="Arial", 
+          fontweight="bold",
+          color="green"
+         )
+
+plt.xlabel("Year",fontsize=20, 
+          family="Arial", 
+          fontweight="bold",
+          color="blue")
+
+plt.ylabel("Students",fontsize=20, 
+          family="Arial", 
+          fontweight="bold",
+          color="blue")
+
+
+
+plt.plot(x, y1)
+plt.plot(x, y2)
+plt.plot(x, y3)
+
+plt.show()
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x= np.array ([2023, 2024, 2025, 2026])
+y1= np.array ([15, 25, 30, 20])
+y2= np.array ([17, 23, 38, 5])
+y3= np.array ([13, 15, 20, 30])
+
+plt.title("Class size", fontsize=20, 
+          family="Arial", 
+          fontweight="bold",
+          color="green"
+         )
+
+plt.xlabel("Year",fontsize=20, 
+          family="Arial", 
+          fontweight="bold",
+          color="blue")
+
+plt.ylabel("Students",fontsize=20, 
+          family="Arial", 
+          fontweight="bold",
+          color="blue")
+
+
+
+plt.plot(x, y1)
+plt.plot(x, y2)
+plt.plot(x, y3)
+
+plt.xticks(x)
+plt.show()
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x= np.array ([2023, 2024, 2025, 2026])
+y1= np.array ([15, 25, 30, 20])
+y2= np.array ([17, 23, 38, 5])
+y3= np.array ([13, 15, 20, 30])
+
+plt.title("Class size", fontsize=20, 
+          family="Arial", 
+          fontweight="bold",
+          color="green"
+         )
+
+plt.xlabel("Year",fontsize=20, 
+          family="Arial", 
+          fontweight="bold",
+          color="blue")
+
+plt.ylabel("Students",fontsize=20, 
+          family="Arial", 
+          fontweight="bold",
+          color="blue")
+
+plt.tick_params(axis="both",
+               colors="red")
+
+
+plt.plot(x, y1)
+plt.plot(x, y2)
+plt.plot(x, y3)
+
+plt.xticks(x)
+plt.show()
+
